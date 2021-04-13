d6147596d00d14a30c0351e760a8713873ce3f41
Gurnoor <gurnoork939@gmail.com>
2021-04-13 14:46:57 +0530

    open and close form function
    js file containing function for opening and closing form

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Function To Display Popup
+function openform()
+{
+    document.getElementById("myform").style.display="block";
+}
+//Function to Hide Popup
+function closeform()
+{
+    document.getElementById("myform").style.display="none";
+}
+
